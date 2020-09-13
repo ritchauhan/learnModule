9fb68f5b918a909669a184ae7c979ca474a96110
ritchauhan <ritchauhan@gmail.com>
2020-09-13 10:54:32 +0530

    doubly linklist demo

--- DS-ALGO/linkList/LinkedListDemo.js
+++ DS-ALGO/linkList/LinkedListDemo.js
@@ -23,7 +23,11 @@ class LinkedList {
         }
 
         this.tail.next = newNode
+        console.log(this.tail)
+        console.log(this.head)
         this.tail = newNode
+        console.log(this.tail)
+        console.log(this.head)
         this.length ++
         return this
     }
@@ -35,15 +39,88 @@ class LinkedList {
         }
         prePendNode.next = this.head
         this.head = prePendNode
-        console.log(prePendNode)
+        // console.log(prePendNode)
         this.length++
+    }
+
+    printAll () {
+        let currentNode = this.head
+        let printArr = []
+        while (currentNode !== null) {
+            printArr.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        console.log(printArr)
+        return printArr
+    }
+    //need to be understood
+    insert(data, value) {
+        let nextNode = {};
+        const node = {
+            value : data,
+            next: null
+        }
+        let currentNode = this.head
+        while (currentNode !==null) {
+            if(currentNode.value === value) {             
+                nextNode = currentNode.next;
+                node.next = nextNode
+                currentNode.next = node
+                this.length ++
+                return this
+            }
+            currentNode = currentNode.next
+        }
+        return this
+    }
+
+    remove(value) {
+        let currentNode = this.head
+        let previousNode = {}
+        while(currentNode != null) {
+            if(currentNode.value === value) {
+                if(previousNode.next) {
+                    previousNode.next = currentNode.next
+                    this.length --
+                    return this
+                } else {
+                    this.head = currentNode.next
+                    this.length --
+                    return this
+                } 
+            }
+            previousNode = currentNode
+            currentNode = currentNode.next
+        }
+    }
+
+    // need to unserstand
+    reverse() {
+        let first = this.head
+        let second = first.next
+
+        while (second) {
+            let temp = second.next
+            second.next = first
+            first = second
+            second = temp
+        }
+        this.head.next = null
+        this.head = first
+        return this
     }
 }
 
 let linkedList = new LinkedList(10)
 linkedList.append(5)
 linkedList.append(16)
-linkedList.append(20)
-linkedList.prepend(11)
-linkedList.prepend(21)
-console.log(linkedList)
+// linkedList.append(20)
+// linkedList.prepend(11)
+// linkedList.prepend(21)
+// linkedList.insert(31, 11)
+// linkedList.insert(41, 31)
+// linkedList.remove(21)
+// linkedList.append(100)
+// console.log/(linkedList)
+linkedList.reverse()
+linkedList.printAll()
